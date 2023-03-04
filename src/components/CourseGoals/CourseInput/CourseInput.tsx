259f44c46d4c6e625941a487216d2db5bb5242be
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-03-03 21:23:51 -0600

    dynamic styling with  css modules for style scoping

--- src/components/CourseGoals/CourseInput/CourseInput.tsx
+++ src/components/CourseGoals/CourseInput/CourseInput.tsx
@@ -1,33 +1,34 @@
 import React, { useState, SyntheticEvent, FormEvent } from "react";
 
 import Button from "../../UI/Button/Button";
-import styled from "styled-components";
-//import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-type divProps = { invalid: boolean };
-const FormControl = styled.div<divProps>`
-  margin: 0.5rem 0;
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")};
-  }
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//import styled from "styled-components";
+
+// type divProps = { invalid: boolean };
+// const FormControl = styled.div<divProps>`
+//   margin: 0.5rem 0;
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = (props: { onAddGoal: (item: string) => void }) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -52,10 +53,12 @@ const CourseInput = (props: { onAddGoal: (item: string) => void }) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
